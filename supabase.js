416856c9e467bedd99e848994947ef35dd6f1797
evozago <evozago@gmail.com>
2025-09-24 14:17:10 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const SUPABASE_URL = "https://SUA_URL.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_CHAVE_ANON";
+const SUPABASE_URL = "https://lvgwkktrzizobazaxdty.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2Z3dra3Ryeml6b2JhemF4ZHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MzIzMjYsImV4cCI6MjA3NDMwODMyNn0.EeAG4AAT0Bix6LADh1bl50vFPWS7qzqIL5g_R7G5M7o";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
